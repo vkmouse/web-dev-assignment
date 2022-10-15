eb953c5fe2b668329461dbc27377a6a9470064d4
vkmouse <knight31241@gmail.com>
2022-10-15 08:33:28 +0800

    Split week-4 py into two impl cookie-based-session and encrypted-cookie

---
+++ week-4/py/encrypted-cookie/functions/routes.py
@@ -0,0 +1,43 @@
+import functions
+from flask import Flask, request, session, redirect, render_template
+
+def configureRoutes(app: Flask):
+    @app.route('/')
+    def index():
+        if (session.get('isLogin')):
+            return redirect('/member')
+        else:
+            return render_template('index.html')
+
+    @app.route('/member')
+    def member():
+        if (session.get('isLogin')):
+            return render_template('member.html')
+        else:
+            return redirect('/')
+
+    @app.route('/error')
+    def error():
+        message = request.args.get('message')
+        return render_template('error.html', message=message)
+
+    @app.route('/signin', methods=['POST'])
+    def signin():
+        account = request.form.get('account')
+        password = request.form.get('password')
+        session['isLogin'] = functions.checkLogin(account, password)
+        if session.get('isLogin'):
+            return redirect('/member')
+        else:
+            message = functions.getErrorMessage(account, password)
+            return redirect(f'/error?message={message}')
+
+    @app.route('/signout')
+    def signout():
+        session['isLogin'] = False
+        return redirect('/')
+
+    @app.route('/square/<int:num>')
+    def square(num):
+        result = str(num ** 2)
+        return render_template('square.html', num=num, result=result)
---
+++ week-4/py/encrypted-cookie/tests/test_routes.py
@@ -0,0 +1,126 @@
+import pytest
+from flask import Flask, session
+from flask.testing import FlaskClient
+from functions import *
+from werkzeug.test import TestResponse
+
+@pytest.fixture()
+def app():
+    app = Flask(__name__, template_folder='../templates')
+    app.test_client()
+    configureRoutes(app)
+    app.config.update({
+        'TESTING': True,
+        'SECRET_KEY': 'test'
+    })
+
+    yield app
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+def assertRedirects(response: TestResponse, path: str):
+    assert len(response.history) == 1
+    assert response.history[0].status_code == 302
+    assert response.request.path == path
+    assert response.status_code == 200
+
+def assertContains(response: TestResponse, text: str):
+    assert text in response.get_data().decode('utf-8')
+    assert response.status_code == 200
+
+def setSession(client: FlaskClient, property: str, value: any):
+    with client.session_transaction() as session:
+        session[property] = value
+
+def testError(client: FlaskClient):
+    messages = [
+        '請輸入帳號、密碼', 
+        '帳號、或密碼輸入錯誤'
+    ]
+    for message in messages:
+        response = client.get(
+            path='/error',
+            query_string={ 'message': message }
+        )
+        assertContains(response, message)
+
+def testHome(client: FlaskClient):
+    response = client.get('/')
+    assertContains(response, '歡迎光臨，請輸入帳號密碼')
+
+def testMemberIfLogin(client: FlaskClient):
+    setSession(client, 'isLogin', True)
+    response = client.get('/member')
+    assertContains(response, '歡迎光臨，這是會員頁')
+
+def testRedirectsHomeToMemberIfLogin(client: FlaskClient):
+    setSession(client, 'isLogin', True)
+    response = client.get(
+        path='/', 
+        follow_redirects=True
+    )
+    assertRedirects(response, '/member')
+
+def testRedirectsMemberToHomeIfNotLogin(client: FlaskClient):
+    response = client.get(
+        path='/member', 
+        follow_redirects=True
+    )
+    assertRedirects(response, '/')
+
+def testSigninSuccess(client: FlaskClient):
+    response = client.post(
+        path='/signin',
+        data={ 
+            'account': 'test',
+            'password': 'test'
+        },
+        follow_redirects=True
+    )
+    assertRedirects(response, '/member')
+
+def testSigninWithoutAccountOrPassword(client: FlaskClient):
+    response = client.post(
+        path='/signin',
+        data={ 
+            'account': '',
+            'password': ''
+        },
+        follow_redirects=True
+    )
+    assertRedirects(response, '/error')
+    assertContains(response, '請輸入帳號、密碼')
+
+def testSigninForPasswordMismatchError(client: FlaskClient):
+    response = client.post(
+        path='/signin',
+        data={ 
+            'account': '123',
+            'password': '123'
+        },
+        follow_redirects=True
+    )
+    assertRedirects(response, '/error')
+    assertContains(response, '帳號、或密碼輸入錯誤')
+
+def testSignout(client: FlaskClient):
+    setSession(client, 'isLogin', True)
+    with client:
+        response = client.get(
+            path='/signout',
+            follow_redirects=True
+        )
+        assert session['isLogin'] == False
+    assertRedirects(response, '/')
+
+def testSquare(client: FlaskClient):
+    response = client.get(
+        path='/square/4'
+    )
+    assertContains(response, '16')
