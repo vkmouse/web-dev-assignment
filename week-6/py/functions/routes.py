57680746405cb59c2e43ef15306e12056badde27
vkmouse <knight31241@gmail.com>
2022-10-24 16:22:30 +0800

    feat(week-6): implement signup api with memory based repository

--- week-6/py/functions/routes.py
+++ week-6/py/functions/routes.py
@@ -1,7 +1,8 @@
 import functions
 from flask import Flask, request, session, redirect, render_template
+from repository import UnitOfWork
 
-def configureRoutes(app: Flask):
+def configureRoutes(app: Flask, unitOfWork: UnitOfWork):
     @app.route('/')
     def index():
         if (session.get('isLogin')):
@@ -36,3 +37,14 @@ def configureRoutes(app: Flask):
     def signout():
         session['isLogin'] = False
         return redirect('/')
+
+    @app.route('/signup', methods=['POST'])
+    def signup():
+        name = request.form.get('name')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        success = unitOfWork.memberRepository.addUser(name, username, password)
+        if success:
+            return redirect('/')
+        else:
+            return redirect('/error?message=帳號已經被註冊')
