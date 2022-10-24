d67170d32e468606d35daf811adcb19e737f4d3d
vkmouse <knight31241@gmail.com>
2022-10-24 15:44:47 +0800

    feat(repository): implement memory based member repository

--- week-6/py/repository/memory_repository.py
+++ week-6/py/repository/memory_repository.py
@@ -1,10 +1,23 @@
 from repository.repository import MemberRepository
 class MemoryMemberRepository(MemberRepository):
+    def __init__(self):
+        self.__db = []
+
     def addUser(self, __name: str, __username: str, __password: str) -> bool:
-        return NotImplemented
+        if self.usernameExists(__username):
+            return False
+        self.__db.append({ 
+            'name': __name,
+            'username': __username,
+            'password': __password,
+        })
+        return True
 
     def usernameExists(self, __username: str) -> bool:
-        return NotImplemented
+        return len(list(filter(lambda i: i['username'] == __username, self.__db))) > 0
 
     def getName(self, __username: str) -> str:
-        return NotImplemented
+        temp = list(filter(lambda i: i['username'] == __username, self.__db))
+        if len(temp) > 0:
+            return temp[0]['name']
+        return ''
