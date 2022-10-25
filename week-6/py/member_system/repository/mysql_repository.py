316d5e82281ae74c1a879b9c936ba89bdfa441c9
vkmouse <knight31241@gmail.com>
2022-10-25 12:09:15 +0800

    feat(week-6): update mysql member repository and unit of work This commit set the connection of mysql from repository to unit of work. It can use same connection for multiple repositories. Use 'cnx.cursor()' to create cursor for each cursor requestment. And 'close()' the cursor after finishing query. It can provide more safe to handle 'unread result found' expection. It will throw the expection while closing not the next query.

--- week-6/py/member_system/repository/mysql_repository.py
+++ week-6/py/member_system/repository/mysql_repository.py
@@ -1,51 +1,110 @@
 import mysql.connector
+from mysql.connector.connection import MySQLConnection
 from member_system.core import Member
 from member_system.repository.repository import MemberRepository, UnitOfWork
 
 class MySQLMemberRepository(MemberRepository):
-    def __init__(self, config):
-        self.cnx = mysql.connector.connect(**config)
-        self.cursor = self.cnx.cursor()
+    def __init__(self, cnx: MySQLConnection, debug: bool):
+        self.debug = debug
+        self.cnx = cnx
+        self.__createTable()
+
+    def __del__(self):
+        if self.debug:
+            self.__dropTableIfExists()
 
     def addMember(self, __name: str, __username: str, __password: str) -> bool:
         if self.__usernameExists(__username):
             return False
         query = (
-            'INSERT INTO member (name, username, password) '
-            'VALUES (%s, %s, %s)')
+            'INSERT INTO {} (name, username, password) '
+            'VALUES (%s, %s, %s)'
+        ).format(self.__tableName)
         data = (__name, __username, __password,)
-        self.cursor.execute(query, data)
+        with self.cnx.cursor() as cursor:
+            cursor.execute(query, data)
         self.cnx.commit()
         return True
 
     def getMember(self, __username: str, __password: str) -> Member | None:
         query = (
             'SELECT id, name '
-            'FROM member '
-            'WHERE username=%s AND password=%s')
+            'FROM {} '
+            'WHERE username=%s AND password=%s '
+            'LIMIT 1'
+        ).format(self.__tableName)
         data = (__username, __password,)
-        self.cursor.execute(query, data)
-        row = self.cursor.fetchone()
+        row = None
+        with self.cnx.cursor() as cursor:
+            cursor.execute(query, data)
+            row = cursor.fetchone()
         if row == None:
             return None
         else:
             (id, name,) = row
             return Member(id, name, __username, __password)
 
-    def __usernameExists(self, __username: str):
+    def __createTable(self):
+        with self.cnx.cursor() as cursor:
+            if not self.__tableExists():
+                cursor.execute(self.__createTableStatement)
+            elif self.debug:
+                cursor.execute(self.__dropTableStatement)
+                cursor.execute(self.__createTableStatement)
+
+    def __dropTableIfExists(self):
+        if self.__tableExists():
+            with self.cnx.cursor() as cursor:
+                cursor.execute(self.__dropTableStatement)
+
+    def __usernameExists(self, __username: str) -> bool:
         query = (
             'SELECT COUNT(*) '
-            'FROM member '
-            'WHERE username=%s')
+            'FROM {} '
+            'WHERE username=%s'
+        ).format(self.__tableName)
         data = (__username,)
-        self.cursor.execute(query, data)
-        [(count,)] = self.cursor.fetchall()
-        return count > 0
+        with self.cnx.cursor() as cursor:
+            cursor.execute(query, data)
+            (count,) = cursor.fetchone()
+            return count > 0
+
+    def __tableExists(self) -> bool:
+        with self.cnx.cursor() as cursor:
+            cursor.execute('SHOW TABLES;')
+            isExists = (self.__tableName,) in cursor.fetchall()
+            return isExists
+
+    @property
+    def __tableName(self) -> str:
+        if self.debug:
+            return 'test_member'
+        else:
+            return 'member'
+
+    @property
+    def __createTableStatement(self) -> str:
+        return (
+            'CREATE TABLE {} ('
+            '    id              bigint        NOT NULL  AUTO_INCREMENT,'
+            '    name            varchar(255)  NOT NULL,'
+            '    username        varchar(255)  NOT NULL,'
+            '    password        varchar(255)  NOT NULL,'
+            '    follower_count  int unsigned  NOT NULL  DEFAULT 0,'
+            '    time            datetime      NOT NULL  DEFAULT CURRENT_TIMESTAMP,'
+            '    PRIMARY KEY (id)'
+            ');'
+        ).format(self.__tableName)
+
+    @property
+    def __dropTableStatement(self) -> str:
+        return 'DROP TABLE {};'.format(self.__tableName)
 
 class MySQLUnitOfWork(UnitOfWork):
-    def __init__(self, config):
-        self.__config = config
+    def __init__(self, config, debug=False):
+        self.__debug = debug
+        self.__cnx = mysql.connector.connect(**config)
         UnitOfWork.__init__(self)
 
     def _createMemberRepository(self) -> MemberRepository:
-        return MySQLMemberRepository(self.__config)
+        return MySQLMemberRepository(self.__cnx, self.__debug)
--- week-6/py/member_system/tests/test_repository.py
+++ week-6/py/member_system/tests/test_repository.py
@@ -1,4 +1,5 @@
 from member_system.repository import UnitOfWork, MemoryUnitOfWork
+from member_system.repository.mysql_repository import MySQLUnitOfWork
 
 def repositoryTest(unitOfWork: UnitOfWork):
     assert unitOfWork.memberRepository.getMember('test', 'test') == None
@@ -8,4 +9,14 @@ def repositoryTest(unitOfWork: UnitOfWork):
 
 def testMemoryMemberRepository():
     unitOfWork = MemoryUnitOfWork()
+    repositoryTest(unitOfWork)
+
+def testMySQLMemberRepository():
+    unitOfWork = MySQLUnitOfWork({
+        'user': 'root',
+        'password': '12345678',
+        'host': '192.168.56.102',
+        'database': 'website',
+        'raise_on_warnings': True
+    }, debug=True)
     repositoryTest(unitOfWork)
