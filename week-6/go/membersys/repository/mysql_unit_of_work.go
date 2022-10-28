4ec5e595107b5790d63d25774e0080971771acbb
vkmouse <knight31241@gmail.com>
2022-10-28 15:56:23 +0800

    feat(week-6): update mysql unit of work

--- week-6/go/membersys/repository/mysql_unit_of_work.go
+++ week-6/go/membersys/repository/mysql_unit_of_work.go
@@ -1,21 +1,42 @@
 package repository
 
 import (
+	. "Project/membersys/core"
+
 	"database/sql"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type MySQLUnitOfWork struct {
-	MemberRepository MySQLMemberRepository
+	UnitOfWork
+	config string
+	debug  bool
+}
+
+func (u *MySQLUnitOfWork) Init() {
+	db, _ := sql.Open("mysql", u.config)
+	mysql := MySQLRepository{db: db, debug: u.debug}
+
+	memberRepository := new(MySQLMemberRepository)
+	memberRepository.MySQLRepository = mysql
+	messageRepository := new(MySQLMessageRepository)
+	messageRepository.MySQLRepository = mysql
+	messageRepository.memberTableName = memberRepository.TableName()
+
+	u.MemberRepository = memberRepository
+	u.MessageRepository = messageRepository
+
+	if u.debug {
+		messageRepository.DropTable(messageRepository)
+		memberRepository.DropTable(memberRepository)
+		memberRepository.CreateTable(memberRepository)
+		messageRepository.CreateTable(messageRepository)
+	}
 }
 
 func NewMySQLUnitOfWork(config string, debug bool) *MySQLUnitOfWork {
-	db, _ := sql.Open("mysql", config)
-
-	unitOfWork := MySQLUnitOfWork{
-		MemberRepository: MySQLMemberRepository{MySQLRepository{db: db, debug: debug}},
-	}
-
+	unitOfWork := MySQLUnitOfWork{config: config, debug: debug}
+	InitUnitOfWork(&unitOfWork)
 	return &unitOfWork
 }
