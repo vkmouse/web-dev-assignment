35952dc2e5d338525b1a570c0f1da76534e01dcb
vkmouse <knight31241@gmail.com>
2022-10-28 16:28:32 +0800

    feat(week-6): update main unit of work to mysql based

--- week-6/go/main.go
+++ week-6/go/main.go
@@ -7,7 +7,8 @@ import (
 
 func main() {
 	router := Router{}
-	unitOfWork := NewMemoryUnitOfWork()
+	// unitOfWork := NewMemoryUnitOfWork()
+	unitOfWork := NewMySQLUnitOfWork("config.json", false)
 	router.Setup("templates", "public", &unitOfWork.UnitOfWork)
 	router.Engine.Run(":8080")
 }
