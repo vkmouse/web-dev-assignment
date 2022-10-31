6ea7a3384daa9683f6fb131d590d507bc4cc8f5f
vkmouse <knight31241@gmail.com>
2022-10-31 21:33:47 +0800

    feat(week-7): add handle update name method

--- week-7/py/public/js/member.js
+++ week-7/py/public/js/member.js
@@ -1,6 +1,6 @@
 function handleQueryMemberName() {
-    const inputElement = document.getElementById('query_member_name_input') // value
-    const outputElement = document.getElementById('query_member_name_output') // textContent
+    const inputElement = document.getElementById('query_member_name_input')
+    const outputElement = document.getElementById('query_member_name_output')
     const username = inputElement.value
     fetch(`/api/member?username=${username}`)
     .then(response => response.json())
@@ -10,5 +10,22 @@ function handleQueryMemberName() {
             output = `${body.data.name} (${body.data.username})`
         }
         outputElement.textContent = output
+    })
+}
+
+function handleUpdateName() {
+    const inputElement = document.getElementById('update_name_input')
+    const outputElement = document.getElementById('update_name_output')
+    const newName = inputElement.value
+    fetch('/api/member', { 
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ 'name': newName }),
+    })
+    .then(response => response.json())
+    .then(body => {
+        if (body.ok) {
+            outputElement.textContent = '更新成功'
+        }
     })
 }
