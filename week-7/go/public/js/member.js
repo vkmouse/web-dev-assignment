827f267213d1f1316fd8bb0b282a79de6084e7c6
vkmouse <knight31241@gmail.com>
2022-11-01 11:35:06 +0800

    feat(week-7): implement query member name modify name frontend

---
+++ week-7/go/public/js/member.js
@@ -0,0 +1,33 @@
+function handleQueryMemberName() {
+    const inputElement = document.getElementById('query_member_name_input')
+    const outputElement = document.getElementById('query_member_name_output')
+    const username = inputElement.value
+    fetch(`/api/member?username=${username}`)
+    .then(response => response.json())
+    .then(body => {
+        let output = ''
+        if (body.data !== null) {
+            output = `${body.data.name} (${body.data.username})`
+        }
+        outputElement.textContent = output
+    })
+}
+
+function handleUpdateName() {
+    const nameElement = document.getElementById('name')
+    const inputElement = document.getElementById('update_name_input')
+    const outputElement = document.getElementById('update_name_output')
+    const newName = inputElement.value
+    fetch('/api/member', { 
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ 'name': newName }),
+    })
+    .then(response => response.json())
+    .then(body => {
+        if (body.ok) {
+            nameElement.textContent = newName
+            outputElement.textContent = '更新成功'
+        }
+    })
+}
